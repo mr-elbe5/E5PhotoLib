4f7211f70869988eb3ea6aa05c497ad8edb23413
miro <miro@elbe5.de>
2024-07-10 13:42:37 +0200

    source fix

--- Package.swift
+++ Package.swift
@@ -12,10 +12,15 @@ let package = Package(
             name: "E5PhotoLib",
             targets: ["E5PhotoLib"]),
     ],
+    dependencies: [
+        .package(
+            url: "https://github.com/mr-elbe5/E5Data",
+            from: "1.0.0")
+    ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "E5PhotoLib"),
+            name: "E5PhotoLib", dependencies: ["E5Data"]),
     ]
 )
